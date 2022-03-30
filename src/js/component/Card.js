a1596fc134d991c913aad8c39a4a44060a530fae
josemiguelhseptien <josemiguelhseptien@gmail.com>
2022-03-30 17:02:53 +0000

    save to submit

---
+++ src/js/component/Card.js
@@ -0,0 +1,83 @@
+import React from "react";
+export const Card = () => {
+	return (
+		<div class="allCards">
+			<div class="card">
+				<img
+					src="http://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Bronchiolar_epithelium_3_-_SEM.jpg/585px-Bronchiolar_epithelium_3_-_SEM.jpg"
+					class="card-img-top"
+					alt="..."
+				/>
+				<div class="card-body">
+					<h5 class="card-title">
+						Scanning electron microscope (SEM)
+					</h5>
+					<p class="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nullam dignissim est quis justo faucibus vulputate.
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div class="card">
+				<img
+					src="https://d2jx2rerrg6sh3.cloudfront.net/image-handler/ts/20110912060643/ri/590/picture/a%20glowing%20cat_1.jpg"
+					class="card-img-top"
+					alt="..."
+				/>
+				<div class="card-body">
+					<h5 class="card-title">
+						Genetically modified ‘glow cat’ - GFP experiment
+					</h5>
+					<p class="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nullam dignissim est quis justo faucibus vulputate.
+						Integer sem felis, rhoncus id faucibus quis, malesuada
+						sit amet tortor
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div class="card">
+				<img
+					src="https://www.acs.org/content/dam/acsorg/molecule/archive-new/t/thalidomide-3d.png"
+					class="card-img-top"
+					alt="..."
+				/>
+				<div class="card-body">
+					<h5 class="card-title">Thalidomide</h5>
+					<p class="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nullam dignissim est quis justo faucibus vulputate.
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div class="card">
+				<img
+					src="https://d2cbg94ubxgsnp.cloudfront.net/Pictures/2000xAny/6/5/9/144659_shutterstock_279819941.jpg"
+					class="card-img-top"
+					alt="..."
+				/>
+				<div class="card-body">
+					<h5 class="card-title">Buckminsterfullerene</h5>
+					<p class="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nullam dignissim est quis justo faucibus vulputate.
+						Integer sem felis, rhoncus id faucibus quis, malesuada
+						sit amet tortor
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
